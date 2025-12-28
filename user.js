ca2bba959f656533ca2d2c7e7b24fa5fd1bafa04
kent082003 <kent082003@gmail.com>
2025-12-28 11:55:12 +0700

    Add files via upload

--- user.js
+++ user.js
@@ -1030,14 +1030,15 @@ window.generateResults = async function () {
         intellectual: await loadMeaning("tu_duy_ly_tri", intellectual),
         subconscious: await loadMeaning("suc_manh_tiem_thuc", subconscious),
         soulPersonalityConnection: await loadMeaning("ket_noi_linhhon_nhancach", soulPersonalityConnection),
-		147: await loadMeaning("mui_ten", "147"),
-		258: await loadMeaning("mui_ten", "258"),
-		369: await loadMeaning("mui_ten", "369"),
-		123: await loadMeaning("mui_ten", "123"),
-		456: await loadMeaning("mui_ten", "456"),
-		789: await loadMeaning("mui_ten", "789"),
-		159: await loadMeaning("mui_ten", "159"),
-		357: await loadMeaning("mui_ten", "357"),
+	"147": await loadMeaning("mui_ten", "147"),
+  "258": await loadMeaning("mui_ten", "258"),
+  "369": await loadMeaning("mui_ten", "369"),
+  "123": await loadMeaning("mui_ten", "123"),
+  "456": await loadMeaning("mui_ten", "456"),
+  "789": await loadMeaning("mui_ten", "789"),
+  "159": await loadMeaning("mui_ten", "159"),
+  "357": await loadMeaning("mui_ten", "357")
+		
 
 
 	
